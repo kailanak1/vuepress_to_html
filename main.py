dd8ad662136d6fd73ddb20edf790a2154c195aca
Kailana Kahawaii <57549694+kailanak1@users.noreply.github.com>
2022-12-13 14:11:51 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import markdown
 import os
+import re
 
 yourpath = "/path/to/directory"
 article_files = []
@@ -17,25 +18,35 @@ def nth_repl_all(s, sub, repl, nth):
     # find + len(sub) + 1 means we start after the last match
         find = s.find(sub, find + len(sub) + 1)
         i += 1
-    repl_all(s, sub, repl)
+    repl_all(s)
 
 
-def repl_all(s, sub, repl):
+def write_file(s):
+    html_title = s.partition('\n')[0]
+    h_title = re.sub('<[^<]+?>', '', html_title)
+    title = re.sub(r'[^\w_. -]', '_', h_title)
+    with open(f'{title}.txt', 'w+') as f:
+        f.write(s)
+
+
+def repl_all(s):
     s = s.replace(":::", "<div>")
-    print(s)
+    write_file(s)
 
 
 def convert(files):
     if len(article_files) != 0:
         for article_path in article_files:
             with open(os.path.expanduser(f"{article_path}")) as article:
-                article = article.read()
-                html_article = markdown.markdown(article)
+                opened_article = article.read()
+                html_article = markdown.markdown(opened_article)
                 # convert ::: to <div>stuff</div>
+                article.close()
                 if html_article.find(':::'):
                     nth_repl_all(html_article, ':::', '</div>', 2)
                 else:
-                    print(html_article)
+                    # write_file(html_article)
+                    print("skip")
 
 
 for root, dirs, files in os.walk(yourpath, topdown=False):
